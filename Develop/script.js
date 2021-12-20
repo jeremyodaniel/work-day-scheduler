89e95f1ad7a1e084e1a03a65980b9f8b9fefca2a
Jeremy O'Daniel <jeremy.odaniel@gmail.com>
2021-12-20 00:03:47 -0600

    add saveBtn functionality so textArea saves to local storage

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,14 @@
-//the current day is displayed at the top of the calendar
-var currentDate = moment().format("dddd, MMMM Do, YYYY");
-$("#currentDay").html(currentDate);
+$(document).ready(function() {
+//  the current day is displayed at the top of the calendar
+    var currentDate = moment().format("dddd, MMMM Do, YYYY");
+    $("#currentDay").html(currentDate);
+});
 
 
 //WHEN I scroll down
 //THEN I am presented with time blocks for standard business hours
+//WHEN I click into a time block
+//THEN I can enter an event
  var currentHour = moment().hours();
 
 $(".time-block").each(function(index) {
@@ -23,14 +27,22 @@ $(".time-block").each(function(index) {
     }
 });
 
+//  WHEN I click the save button for that time block
+//  THEN the text for that event is saved in local storage
+var saveBtn = $(".saveBtn");
+
+$(saveBtn).on("click", function() {
+    var text = $(this).siblings(".plan").val(); 
+    var time = $(this).parent().attr("id");
+    localStorage.setItem(time, text);
+
+});
+
 //WHEN I view the time blocks for that day
 //THEN each time block is color-coded to indicate whether it is in the past, present, or future
 
-//WHEN I click into a time block
-//THEN I can enter an event
 
-//WHEN I click the save button for that time block
-//THEN the text for that event is saved in local storage
+
 
 
 //WHEN I refresh the page
